e387ac38a06cc2e5d14c6a15c1e2025bf7e0d493
Cℓementine♥ <53567265+Clementine696@users.noreply.github.com>
2020-04-19 01:05:17 +0700

    Update Pookhao.c

--- Pookhao.c
+++ Pookhao.c
@@ -1,38 +1,36 @@
 #include<stdio.h>
 main()
 {
-    int mountain_num,i,j,max=0,row,c,min;
+    int mountain_num,i,j,maxhigh=0,row,c;
     scanf("%d",&mountain_num);
     int take[mountain_num];
     for(i=0;i<mountain_num;i++)
     {
         scanf("%d",&take[i]);
-        if(take[i]>max)
-            max=take[i];
+        if(take[i]>maxhigh)
+            maxhigh=take[i];
     }
-    min=max;
-    for(row=0;row<max;row++)
+    for(row=0;row<maxhigh;row++)
     {
         for(i=0;i<mountain_num;i++)
         {
-            if(take[i]<min)
+            if(take[i]<maxhigh-row)
             {
                 for(c=0;c<take[i]*2;c++)
                     printf(" ");
             }
-            if(take[i]>=min)
+            if(take[i]>=maxhigh-row)
             {
-                for(c=1;c<min;c++)
+                for(c=1;c<maxhigh-row;c++)
                     printf(" ");
                 printf("/");
-                for(c=take[i];c>min;c--)
+                for(c=take[i];c>maxhigh-row;c--)
                     printf("  ");
                 printf("\\");
-                for(c=1;c<min;c++)
+                for(c=1;c<maxhigh-row;c++)
                     printf(" ");
             }
         }
         printf("\n");
-        min=min-1;
     }
 }
