652c4c8c0ff6d327c6b1cf46443a6784b8ef099a
Cℓementine♥ <53567265+Clementine696@users.noreply.github.com>
2020-04-19 00:46:36 +0700

    Add files via upload

---
+++ Pookhao.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+main()
+{
+    int num,i,j,max=0,sum=0,row,c,min;
+    scanf("%d",&num);
+    int take[num];
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&take[i]);
+        sum+=take[i];
+        if(take[i]>max)
+            max=take[i];
+    }
+    min=max;
+    for(row=0;row<max;row++)
+    {
+        for(i=0;i<num;i++)
+        {
+            if(take[i]<min)
+            {
+                for(c=0;c<take[i]*2;c++)
+                    printf(" ");
+            }
+            if(take[i]>=min)
+            {
+                for(c=1;c<min;c++)
+                    printf(" ");
+                printf("/");
+                for(c=take[i];c>min;c--)
+                    printf("  ");
+                printf("\\");
+                for(c=1;c<min;c++)
+                    printf(" ");
+            }
+        }
+        printf("\n");
+        min=min-1;
+    }
+}
